5c8c43b8a301cff60a475c234fb9a2b4b1c2fc64
littlecharacter <littlecharacter@qq.com>
2020-01-14 00:45:24 +0800

    msf client modify

--- msf-client/src/main/java/com/lc/msf/client/ClientHandler.java
+++ msf-client/src/main/java/com/lc/msf/client/ClientHandler.java
@@ -8,12 +8,14 @@ public class ClientHandler extends ChannelHandlerAdapter {
     private Object response;
 
     public Object getResponse() {
+        // hung住等待rpc返回结果
         return response;
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         try {
+            // 得到结果唤醒等待结果的线程
             response = msg;
         } finally {
             ReferenceCountUtil.release(msg);
